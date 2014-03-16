c054d8239f3a6e187ececc3a847fbcca49d249c3
Shane Butlion <shanebutlion@Shanes-MacBook-Air.local>
2014-03-16 16:52:56 +0200

    Added Test Class

---
+++ Git Test/SBTest.h
@@ -0,0 +1,13 @@
+//
+//  SBTest.h
+//  Git Test
+//
+//  Created by Shane Butlion on 2014/03/16.
+//  Copyright (c) 2014 Shane Butlion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBTest : NSObject
+
+@end
---
+++ Git Test/SBTest.m
@@ -0,0 +1,13 @@
+//
+//  SBTest.m
+//  Git Test
+//
+//  Created by Shane Butlion on 2014/03/16.
+//  Copyright (c) 2014 Shane Butlion. All rights reserved.
+//
+
+#import "SBTest.h"
+
+@implementation SBTest
+
+@end
